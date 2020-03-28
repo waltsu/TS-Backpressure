a35ec5d887cd9da2d248a2b7f4fc3f89b061dc76
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2020-03-28 19:49:02 +0200

    Add failing test
    The test should pass when the third call waits until the second call is
    executed. This means that the difference between done timestamps should
    be greater than 500ms

--- src/backpressure.spec.ts
+++ src/backpressure.spec.ts
@@ -1,5 +1,26 @@
+import { Backpressure } from './backpressure';
+
 describe('backpressure', () => {
-  it('runs the test', () => {
-    expect(true).toBeTruthy();
+  it('only allows 2 inflight calls of function', async () => {
+    const backpressure = new Backpressure();
+    const sleeper = async (callback: Function) => {
+      await new Promise(resolve => setTimeout(resolve, 500));
+      callback(Date.now());
+    };
+    const backpressuredSleeper = backpressure.wrap(sleeper);
+
+    const doneTrackers = Array.from(Array(3)).map(() => jest.fn());
+    const calls = doneTrackers.map(isDone => {
+      return backpressuredSleeper(isDone);
+    });
+
+    await Promise.all(calls);
+
+    const doneTimestamps = doneTrackers
+      .map(jestFn => {
+        return jestFn.mock.calls[0][0];
+      })
+      .sort();
+    expect(doneTimestamps[2] - doneTimestamps[1]).toBeGreaterThan(500);
   });
 });
