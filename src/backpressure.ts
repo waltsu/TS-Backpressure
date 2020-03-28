09a8ddc6e42fc698f23271989c6fa1052b07a5f5
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2020-03-28 20:15:52 +0200

    Require that backpressured function returns a promise

--- src/backpressure.ts
+++ src/backpressure.ts
@@ -1,15 +1,21 @@
+type BackpressuredFunction<T> = (...args: any[]) => Promise<T>;
+
 export class Backpressure {
   private inflightCalls = 0;
   /**
-   * Returns function whose execution is delayed if there is too many inflight calls
+   * Returns Promise whose execution is delayed if there is too many inflight calls
    */
-  public wrap<T extends CallableFunction>(fn: T): T {
-    const backpressuredFunction = (...args: any[]) => {
+  public wrap<T>(fn: BackpressuredFunction<T>): BackpressuredFunction<T> {
+    const backpressuredFunction = async (...args: any[]) => {
       this.inflightCalls++;
-      const returnValue = fn(...args);
-      this.inflightCalls--;
 
-      return returnValue;
+      try {
+        console.log(this.inflightCalls);
+        const returnValue = await fn(...args);
+        return returnValue;
+      } finally {
+        this.inflightCalls--;
+      }
     };
     return backpressuredFunction as any;
   }
