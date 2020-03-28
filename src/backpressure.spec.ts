3dc0541695300b2d3f2b373cd1e17b0b636d7998
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2020-03-28 21:20:12 +0200

    Add test cases to test that failures won't affect the backpressuring

--- src/backpressure.spec.ts
+++ src/backpressure.spec.ts
@@ -1,10 +1,12 @@
 import { Backpressure } from './backpressure';
 
+const SLEEP_TIME_MS = 100;
+
 describe('backpressure', () => {
-  it('only allows 2 inflight calls of function', async () => {
-    const backpressure = new Backpressure();
+  it('only allows N concurrect calls of a function', async () => {
+    const backpressure = new Backpressure(2);
     const sleeper = async (callback: Function) => {
-      await new Promise(resolve => setTimeout(resolve, 500));
+      await new Promise(resolve => setTimeout(resolve, SLEEP_TIME_MS));
       callback(Date.now());
     };
     const backpressuredSleeper = backpressure.wrap(sleeper);
@@ -21,6 +23,26 @@ describe('backpressure', () => {
         return jestFn.mock.calls[0][0];
       })
       .sort();
-    expect(doneTimestamps[2] - doneTimestamps[1]).toBeGreaterThan(500);
+    expect(doneTimestamps[2] - doneTimestamps[1]).toBeGreaterThan(SLEEP_TIME_MS);
+  });
+
+  it('allows functions to fail', async () => {
+    const backpressure = new Backpressure(1);
+    const maybeThrow = async (shouldThrow: boolean) => {
+      await new Promise(resolve => setTimeout(resolve, SLEEP_TIME_MS));
+      if (shouldThrow) throw new Error('boom');
+      return 'success';
+    };
+    const backpressuredFunction = backpressure.wrap(maybeThrow);
+
+    const shouldThrows = Array.from(Array(4)).map((_, i) => (i % 2 === 0 ? true : false));
+    const calls = shouldThrows.map(shouldThrow => {
+      return backpressuredFunction(shouldThrow).catch(() => 'failed');
+    });
+
+    const results = await Promise.all(calls);
+
+    expect(results.filter(result => result === 'success').length).toEqual(2);
+    expect(results.filter(result => result === 'failed').length).toEqual(2);
   });
 });
