911c911817de521cb1d6542bd72b2d2e811618a1
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2020-03-28 20:07:02 +0200

    Start tracking of inflight calls

--- src/backpressure.ts
+++ src/backpressure.ts
@@ -1,5 +1,16 @@
-export function greet(): string {
-  return 'hello';
-}
+export class Backpressure {
+  private inflightCalls = 0;
+  /**
+   * Returns function whose execution is delayed if there is too many inflight calls
+   */
+  public wrap<T extends CallableFunction>(fn: T): T {
+    const backpressuredFunction = (...args: any[]) => {
+      this.inflightCalls++;
+      const returnValue = fn(...args);
+      this.inflightCalls--;
 
-console.log(greet());
+      return returnValue;
+    };
+    return backpressuredFunction as any;
+  }
+}
