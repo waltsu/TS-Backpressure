bcbf5c19fe5fc53aa2eb493dbd757a3e247f1edc
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2020-03-29 15:39:31 +0300

    Add failing integration test for koa middleware

---
+++ src/koa-middleware.integration.spec.ts
@@ -0,0 +1,46 @@
+import * as Koa from 'koa';
+import axios from 'axios';
+import { Server } from 'http';
+import { AddressInfo } from 'net';
+
+const SLEEP_TIME_MS = 100;
+const sleepEndpoint = async (ctx: Koa.Context, _next: Koa.Next) => {
+  await new Promise(resolve => setTimeout(resolve, SLEEP_TIME_MS));
+  ctx.body = 'done';
+};
+
+const createTestApp = () => {
+  const app = new Koa();
+  app.use(sleepEndpoint);
+
+  const server = app.listen(0);
+  const address = server.address() as AddressInfo;
+
+  return { app, server, address };
+};
+
+describe('koa-middleware', () => {
+  let server: Server;
+  let port: number;
+
+  beforeAll(() => {
+    const app = createTestApp();
+    server = app.server;
+    port = app.address.port;
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('applies backpressure', async () => {
+    const now = Date.now();
+    const responses = await Promise.all(
+      Array.from(Array(3)).map(() => axios.get(`http://localhost:${port}`))
+    );
+    const elapsed = Date.now() - now;
+
+    responses.forEach(response => expect(response.status).toEqual(200));
+    expect(elapsed).toBeGreaterThanOrEqual(3 * SLEEP_TIME_MS);
+  });
+});
